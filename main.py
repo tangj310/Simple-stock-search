83897d429dd88cf1cd1c5e3d407fa7c69c9e709a
JerryT310 <jerry.tang@rci.rogers.ca>
2024-01-03 09:38:39 -0500

    dev stock search

--- main.py
+++ main.py
@@ -6,46 +6,13 @@ from stock_report import __get_stock_data, __period_metric_perc
 
 app = FastAPI()
 
-
-class StockAnalysisRequest(BaseModel):
-    ticker_symbol: str
-    time_period: int
-
-@app.get("/")
-def stock_analysis(request: StockAnalysisRequest):
-
-    today_date = date.today()
-    search_start_date = today_date - timedelta(request.time_period)
-    search_end_date = today_date
-
-
-    df = __get_stock_data(
-
-        request.ticker_symbol
-        ,search_end_date - timedelta(1)
-        ,search_end_date
-    )
-
-    metric_name_list = [
-        'Volume'
-        ,'Close'
-    ]
-
-    metric_dict = __period_metric_perc(df, metric_name_list)
-
+@app.get('/')
+def index():
     return {
-        'start_date': str(search_start_date)
-        ,'end_date': str(search_end_date)
-        ,'stock_symbol': request.ticker_symbol.upper()
-        ,'date': df.to_dict(orient='records')
-        ,'metrics': metric_dict
+        'data': 'stock_list'
     }
 
-if __name__ == '__main__':
-    import uvicorn
 
-    uvicorn.run(
-        app
-        ,host="127.0.0.1"
-        ,port=8000
-        )
+@app.get('/stock/{ticker}')
+def show(ticker: str):
+    return {'data': ticker}
